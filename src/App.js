67769a844b9dda157de5c626d16ac12efae6f9ac
David <david.perez.campos82@gmail.com>
2024-06-04 00:57:17 +0200

    Icons & backgrounds updated

--- src/App.js
+++ src/App.js
@@ -51,7 +51,36 @@ const App = () => {
             onClick={handleSectionClick} 
             isActive={clickedSection === "Tools"}
           >
-            Tools content
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div style={{ flex: 1 }}>
+                <h1>Languages</h1>
+                <a href="#">Spanish <img width="90" height="15" src="/images/5stars.png" alt="" /><br></br><br></br></a>
+                <a href="#">English <img width="90" height="15" src="/images/4stars.png" alt="" /></a><br></br><br></br>
+                <h1>Programming</h1>
+                <a href="#">Python <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">Javascript <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">Java <img width="90" height="15" src="/images/3stars.png" /><br></br><br></br></a>
+                <a href="#">VBS <img width="90" height="15" src="/images/3stars.png" /><br></br><br></br></a>          
+              </div>
+              <div style={{ flex: 1 }}>
+                <h1>Tools</h1>
+                <a href="#">Selenium <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">WebDriver <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">UFT <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">Karate <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">Cypress <img width="90" height="15" src="/images/3stars.png" /><br></br><br></br></a>
+                <a href="#">LoadRunner <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">JMeter <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">Gatling <img width="90" height="15" src="/images/2stars.png" /><br></br><br></br></a>
+              </div>
+              <div style={{ flex: 1 }}>
+                <h1>Others</h1>
+                <a href="#">Azure Devops <img width="90" height="15" src="/images/4stars.png" /><br></br><br></br></a>
+                <a href="#">AWS <img width="90" height="15" src="/images/3stars.png" /><br></br><br></br></a>
+                <a href="#">Docker <img width="90" height="15" src="/images/3stars.png" /><br></br><br></br></a>
+                <a href="#">Kubernetes <img width="90" height="15" src="/images/3stars.png" /><br></br><br></br></a>          
+              </div>
+            </div>
           </Section>
           <Section 
             title="Certifications" 
